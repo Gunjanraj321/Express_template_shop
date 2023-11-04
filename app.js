f04dc8867ea147501d5bbc5dd36ce0d28de51c22
Gunjan Kumar <gunjanraj321@gmail.com>
2023-11-04 14:24:40 +0530

    some change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -21,4 +21,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(3001);
