9daf75c9746ffdc27dc7c08f68fe66a313cb6557
Gunjan Kumar <gunjanraj321@gmail.com>
2023-11-04 21:50:51 +0530

    some bug

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing :false
+    editing: false
   });
 };
 
@@ -13,31 +13,38 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
+exports.postDeleteProduct = (res, req, next)=>{
+  const prodId = req.body.productId;
+
+  Product.deleteById(prodId);
+  res.redirect('/admin/products')
+}
+
 exports.getEditProduct = (req, res, next) => {
-  const  editMode = req.query.edit;
-  if(!editMode){
+  const editMode = req.query.edit;
+  if (!editMode) {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product =>{
-    if(!product){
-      return res.redirect('/')
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product:product
+      product: product
     });
-  })
+  });
 };
 
-exports.postEditProduct = (req,res,next)=>{
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
@@ -49,14 +56,10 @@ exports.postEditProduct = (req,res,next)=>{
     updatedImageUrl,
     updatedDesc,
     updatedPrice
-    )
-    updatedProduct.save()
-    res.redirect('/admin/products')
-}
-
-// exports.getDeleteProduct = (req,res, next)=>{
-  
-// }
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -18,8 +19,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(id,title, imageUrl, description, price) {
-    this.id=id;
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -28,21 +29,35 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
-      if(this.id){
-        const existingProductIndex = products.findIndex(prod=>prod.id === this.id)
-        const updatedProducts =[...products]
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        );
+        const updatedProducts = [...products];
         updatedProducts[existingProductIndex] = this;
         fs.writeFile(p, JSON.stringify(updatedProducts), err => {
           console.log(err);
         });
-      } else{ 
-      this.id = Math.random().toString();
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
       }
+    });
+  }
 
+  static deleteById(id){
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id ===id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p , JSON.stringify(updatedProducts),err=>{
+          if(!err){
+            Cart.deleteProduct(id,product.price)  
+          }
+      })
+      cb(product);
     });
   }
 
@@ -55,5 +70,22 @@ module.exports = class Product {
       const product = products.find(p => p.id === id);
       cb(product);
     });
+  }
+
+  static deleteProduct(id,productPrice){
+    fs.readFile(p,(err,fileContent)=>{
+      if(err) {
+        return;
+      }
+      const updatedCart = {...JSON.stringify(fileContent)}
+      const product = updatedCart.products.find(prod => prod.id ===id)
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod =>prod.id!==id)
+      updatedCart.totalPrice=updatedCart.totalPrice-productPrice*productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    })
   }
 };
