d9dfb5353ad72345e0e5ec0240af8ec738e23f4e
Gunjan Kumar <gunjanraj321@gmail.com>
2023-11-13 18:49:16 +0530

    some update

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product= require('./models/product')
+const User = require("./models/user")
 
 const app = express();
 
@@ -17,15 +19,33 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req , res, next) =>{
+    User.findOne({ where: { id: 1 }}).then(user=>{
+        req.user = user;
+    }).catch(err=>console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result =>{
-    //console.log(result)
-    app.listen(3000);
+Product.belongsTo(User,{constraints : true , onDelete :'CASCADE'});
+User.hasMany(Product);
+
+
+sequelize.sync()
+    .then(result =>{
+        User.findOne({ where: { id: 1 }});
 })
+    .then(user=>{
+        if(!user) {
+            User.create({name:'max',emails:'test@gmail.com'});
+        }
+        return user;
+    }).then(user => {
+       // console.log(user);
+        app.listen(3000);
+    })
 .catch(err => console.log(err))
 
 
--- util/database.js
+++ util/database.js
@@ -1,14 +1,3 @@
-// const mysql = require('mysql2');
-
-// const pool = mysql.createPool({
-//     host : 'localhost',
-//     user : 'root',
-//     database : 'Products',
-//     password : '12345678'
-// })
-
-// module.exports = pool.promise();
-
 const Sequelize = require("sequelize")
 
 const sequelize = new Sequelize('Products','root','12345678', {
